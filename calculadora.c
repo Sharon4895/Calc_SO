206453672eb75f4712c4185e7828ee1a3e8e80c0
marielahr <160739768+marielahr@users.noreply.github.com>
2024-02-22 12:59:33 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,10 @@
 #import <stdio.h>
+
+int main ()
+{
+  printf (" CALCULADORA ");
+  printf ( " Selecciona una opcion: ");
+  printf ("1. SUMA");
+  printf ("2. RESTA");
+  printf ("3. MULTIPLICACION");
+  printf ("4. DIVISION");
