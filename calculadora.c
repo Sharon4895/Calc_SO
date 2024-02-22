fcc45ffaa1d625864127bdc2f43ae2e2692c240b
Sharon4895 <117042188+Sharon4895@users.noreply.github.com>
2024-02-22 13:19:35 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,15 +1,20 @@
 #import <stdio.h>
 
+void suma();
+void resta();
+void mult();
+void divi();
+
 int main ()
 {
   int selecc=0;
   printf (" CALCULADORA ");
-  printf ( " Selecciona una opcion: ");
-  scanf("%i",&selecc);
+  printf ("\nSelecciona una opcion: ");
   printf ("\n1. SUMA");
   printf ("\n2. RESTA");
   printf ("\n3. MULTIPLICACION");
-  printf ("\n4. DIVISION");
+  printf ("\n4. DIVISION\nSeleccion: ");
+  scanf("%i",&selecc);
 
   switch(selecc){
     case(1):
@@ -25,4 +30,24 @@ int main ()
       div();
       break;
   }
+}
+
+void mult(){
+  float n1,n2,res;
+  printf("\nIngresa el primer valor: ");
+  scanf("%f",&n1);
+  printf("\nIngresa el segundo valor: ");
+  scanf("%f",&n2);
+  res=n1*n2;
+  printf("\nResultado: %f",res);
+}
+
+void div(){
+  float n1,n2,res;
+  printf("\nIngresa el primer valor: ");
+  scanf("%f",&n1);
+  printf("\nIngresa el segundo valor: ");
+  scanf("%f",&n2);
+  res=n1/n2;
+  printf("\nResultado: %f",res);
 }
